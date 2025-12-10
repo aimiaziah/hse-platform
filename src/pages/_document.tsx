ae2267f4f53c60305342f9da8a599ddeba8ce630
Aimi <Aimi>
2025-12-10 16:53:47 +0800

    fix: resolve TypeScript isolatedModules errors in multiple files

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,9 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-import React from 'react';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head>
-        {/* PWA and SEO meta tags are handled in _app.tsx and layouts */}
-        {/* Additional document-level head elements can go here */}
-      </Head>
+    <Html>
+      <Head />
       <body>
         <Main />
         <NextScript />
